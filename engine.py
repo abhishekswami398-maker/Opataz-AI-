f163e233020b0cf3c668d67bba8c96dc6c35fee2
abhishekswami398-maker <abhishekswami398@gmail.com>
2026-06-18 12:56:15 +0530

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,10 @@
+class OpatazEngine:
+
+    @staticmethod
+    def calculate():
+        return {
+            "time": 0,
+            "O_prime": 0,
+            "O_final": 0,
+            "rang_box": []
+        }
